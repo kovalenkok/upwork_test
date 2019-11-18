64a6a2ed49ded9a4627ccbe87cab6d1614e46a5c
kovalenkok <kostiantyn2_koval@outlook.com>
2019-11-18 21:50:45 +0800

    Added some dependencies for test purpose

--- app/src/main/java/com/app/upworktest/activities/MainActivity.java
+++ app/src/main/java/com/app/upworktest/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.app.upworktest.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup support action bar.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.app_name);
+        }
     }
 }
